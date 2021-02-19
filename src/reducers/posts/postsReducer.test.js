ef69999c28afba7259fbd4b7cbf67a78ed5adf9a
karankumarshreds <naileditnerd@gmail.com>
2021-02-20 01:20:02 +0530

    add reducer test

--- src/reducers/posts/postsReducer.test.js
+++ src/reducers/posts/postsReducer.test.js
@@ -3,8 +3,17 @@ import { GET_POSTS } from '../../actions/types';
 import { postsReducer } from './postsReducer';
 
 describe('Posts Reducer', () => {
+    // assertions
     it('Should return default state', () => {
-        const newState = postsReducer(undefined, 'RANDOM_TYPE');
+        // should return default state if type is not matched
+        const newState = postsReducer(undefined, { type: 'RANDOM_TYPE' });
         expect(newState.posts.length).toEqual(0);
+    });
+
+    it('Should return updated state', () => {
+        // mocking the action with payload
+        const action = { type: 'GET_POSTS', payload: ['a', 'b', 'c'] };
+        const updatedState = postsReducer(undefined, action);
+        expect(updatedState.posts.length).toEqual(3);
     });
 });
