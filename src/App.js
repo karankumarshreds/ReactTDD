de7d281f02098f5c9e602b554875b9431a34dc7c
karankumarshreds <naileditnerd@gmail.com>
2021-02-19 20:59:43 +0530

    add header bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App = () => {
-  return <div>App</div>;
+  return <Header />;
 };
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand">Navbar</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
